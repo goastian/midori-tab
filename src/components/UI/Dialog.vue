4e266e23fe9e96adcd1d17340cc8ca13487e78e8
Im-Jota <joseluisromeroarce12@gmail.com>
2025-08-08 10:30:44 -0500

    fix: add z-index to avoid being hidden

--- src/components/UI/Dialog.vue
+++ src/components/UI/Dialog.vue
@@ -86,6 +86,7 @@
   opacity: 0;
   transition: background-color 0.3s ease, opacity 0.3s ease;
   visibility: hidden;
+  z-index: 5;
 }
 
 .containerDialog.show {
@@ -112,6 +113,7 @@
   display: flex;
   flex-direction: column;
   gap: .5rem;
+  z-index: 6;
 }
 
 .dialog.show {
