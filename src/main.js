c2ad65af58dbd7c51d240a9e0b0cb6767d167b03
Alfonso Hernandez <alfonso.hernandez@astian.org>
2026-03-02 00:36:57 -0500

    add new file and apply palette colors

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import persistedState from 'pinia-plugin-persistedstate';
 import '@fontsource/inter/latin-400.css';
+import '@fontsource/inter/latin-500.css';
+import '@fontsource/inter/latin-600.css';
 
+import './styles/tokens.css'
+import './styles/animations.css'
 import './style.css'
 import 'zen-wdg/zen-wdg.css'
-import "gridstack/dist/gridstack.css";
 
 const app = createApp(App)
 const pinia = createPinia();
